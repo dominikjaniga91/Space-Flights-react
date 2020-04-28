44a7a6a5855ff2631a2011fc0a062c06a5eb12b8
Dominik <dominikjaniga91@gmail.com>
2020-04-28 19:50:31 +0200

    Change payload in post login  request

--- src/components/Authentication/Login.js
+++ src/components/Authentication/Login.js
@@ -25,14 +25,17 @@ class Login extends Component {
 
     };
 
-    handleSubmit = () => {
+    handleSubmit = event => {
+        event.preventDefault();
+        const user = {username: this.state.username, password: this.state.password };
 
         fetch('http://localhost:8080/login',{
-            method: 'POST',
-            body: JSON.stringify(this.state)
+            method: "POST",
+            body: JSON.stringify(user)
         })
         .then(response => {
             const token = response.headers.get('Authorization');
+            console.log(token)
             if(token !== null){
                 sessionStorage.setItem("jwt", token);
                 this.setState({isAuthenticated: true});
@@ -46,36 +49,37 @@ class Login extends Component {
 
         if(this.state.isAuthenticated === true) {
             return <Redirect to={'/listOfFlights'} />
+        } else {
+
+            return(
+
+                <div className="mainFormCenter">
+                    <Form className="myForm" onSubmit={this.handleSubmit}>
+                        <Form.Group as={Row} controlId="formPlaintextEmail">  
+                            <Form.Label column sm="4">Login:</Form.Label>
+                            <Col sm="8">
+                            <Form.Control type="text"
+                                name="username"
+                                onChange={this.handleChange}
+                                required />
+                            </Col>
+                        
+                        </Form.Group>
+                        <Form.Group as={Row} controlId="formPlaintextEmail">  
+                            <Form.Label column sm="4">Password:</Form.Label>
+                            <Col sm="8">
+                            <Form.Control type="password"
+                                name="password"
+                                onChange={this.handleChange}
+                                required />
+                            </Col>
+                        </Form.Group>
+                        <MyButton>Login</MyButton>
+                    </Form>
+                </div>
+
+            );
         }
-
-        return(
-
-            <div className="mainFormCenter">
-                <Form className="myForm" onSubmit={this.handleSubmit}>
-                    <Form.Group as={Row} controlId="formPlaintextEmail">  
-                        <Form.Label column sm="4">Login:</Form.Label>
-                        <Col sm="8">
-                        <Form.Control type="text"
-                               name="destination"
-                               onChange={this.handleChange}
-                               required />
-                        </Col>
-                       
-                    </Form.Group>
-                    <Form.Group as={Row} controlId="formPlaintextEmail">  
-                        <Form.Label column sm="4">Password:</Form.Label>
-                        <Col sm="8">
-                        <Form.Control type="password"
-                               name="password"
-                               onChange={this.handleChange}
-                               required />
-                        </Col>
-                    </Form.Group>
-                    <MyButton>Login</MyButton>
-                </Form>
-            </div>
-
-        );
     }
 }
 
