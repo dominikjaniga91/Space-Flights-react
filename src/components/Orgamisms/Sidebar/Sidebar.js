5ea0db457dbe6a776375e6ba1d290ab1f5646e71
Dominik <dominikjaniga91@gmail.com>
2020-05-13 19:55:08 +0200

    Create empty right sidebar

---
+++ src/components/Orgamisms/Sidebar/Sidebar.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const StyledWrapper = styled.div`
+
+    width: 60px;
+    height: 100%;
+    background-color: hsl(214, 7%, 20%);
+    position: fixed;
+    right: 0;
+    top: 0;
+    z-index: 0;
+
+`;
+
+const Sidebar = () => (
+
+    <StyledWrapper />
+);
+
+export default Sidebar;
